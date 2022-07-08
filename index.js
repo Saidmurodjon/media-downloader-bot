e16ad5616736b243a18cc9bdfd160f43925e1a8e
kali <kali@kali.kali>
2022-07-08 10:40:48 +0500

    @

--- index.js
+++ index.js
@@ -5,27 +5,24 @@ const Controllers = require("./Controllers.js");
 // const Telegraf = require('telegraf');
 // const bot = new Telegraf('123:ABC');
 
-bot.telegram.setWebhook("https://saidmurod.uz");
-bot.startWebhook(`/${TOKEN}`, null, 4000);
-bot.on("text", (ctx) => {
-  // Explicit usage
-  ctx.telegram.sendMessage(
-    ctx.message.chat.id,
-    `Hello ${ctx.message.from.username}`
-  );
+// bot.telegram.setWebhook("https://saidmurod.uz");
 
-  // Using context shortcut
-  // ctx.reply(`Hello ${ctx.state.role}`);
-});
-bot.use(function (ctx, next) {
-  try {
-  } catch (e) {
-    console.log("Error");
+bot.command('image', (ctx) => ctx.replyWithPhoto({ url: 'https://picsum.photos/200/300/?random' }))
+bot.on('text', (ctx) => ctx.replyWithHTML('<b>Hello</b>'))
+
+// Start webhook directly
+// bot.startWebhook('/secret-path', null, 3000)
+// bot.telegram.setWebhook('https://---.localtunnel.me/secret-path')
+
+// Start webhook via launch method (preferred)
+bot.launch({
+  webhook: {
+    domain: 'https://saidmurod.uz/',
+    port: 4000
   }
-});
+})
 
-// bot.launch({
-//   webhook: {
-//     domain: "https://saidmurod.uz",
-//   },
-// });
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
