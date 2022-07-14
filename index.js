2a0a5a022bf488fdcfa07dd448049e8ed0db98f4
kali <kali@kali.kali>
2022-07-14 18:28:20 +0500

    $

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const { Telegraf } = require("telegraf");
-const { TOKEN, MONGODB ,PORT} = require("./config.js");
+const { TOKEN, MONGODB, PORT } = require("./config.js");
 const bot = new Telegraf(TOKEN);
 const Controllers = require("./Controllers.js");
+const { Download } = require("./req");
 const express = require("express");
 const app = express();
 
 app.use(bot.webhookCallback("/"));
-bot.telegram.setWebhook("https://c308-37-110-211-20.eu.ngrok.io/");
+bot.telegram.setWebhook("https://nodejs-telegraf-testbot.herokuapp.com");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-bot.on("text", (ctx) => {
-
-  ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.message.from.first_name}`);
-
+bot.on("text", async (ctx) => {
+  try {
+    ctx.telegram.sendMessage(
+      ctx.message.chat.id,
+      `Hello ${ctx.message.from.first_name}`
+    );
+    // console.log(ctx.message);
+    const getVideoUrl = await Download(ctx.message.text);
+    await ctx.telegram.sendVideo(ctx.message.chat.id, getVideoUrl.videoUrl, {
+      caption: getVideoUrl.caption,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
